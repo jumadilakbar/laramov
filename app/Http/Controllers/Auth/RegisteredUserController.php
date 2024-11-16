32c3842f99b9f616a97f9e85206dd43d5ffa13b1
jumadilakbar <muhamadjumadilakbar@gmail.com>
2024-11-16 11:59:59 +0700

    seeder user and roles

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,7 +42,7 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-
+        $user->assignRole('user');
         event(new Registered($user));
 
         Auth::login($user);
